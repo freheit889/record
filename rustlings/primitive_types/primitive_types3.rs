75ac698b97f5d7e9e42d63df17d32eb8aecff653
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 14:30:59 +0800

    Create primitive_types3.rs

---
+++ rustlings/primitive_types/primitive_types3.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a =[0;200];
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
