a7f54470263649c0826ab6d4d9de79d303e36c41
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 14:27:49 +0800

    Create primitive_types1.rs

---
+++ rustlings/primitive_types/primitive_types1.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Booleans (`bool`)
+
+    let is_morning = true;
+    if is_morning {
+        println!("Good morning!");
+    }
+
+    let is_evening=!is_morning;
+    if is_evening {
+        println!("Good evening!");
+    }
+}
