8f864e338fefdedeee5c3610b88efa6b896b6f21
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 13:24:05 +0800

    Create option1.rs

---
+++ rustlings/option/option1.rs
@@ -0,0 +1,16 @@
+fn print_number(maybe_number: Option<u16>) {
+    println!("printing: {}", maybe_number.unwrap());
+}
+
+fn main() {
+    print_number(Some(13));
+    print_number(Some(99));
+
+    let numbers=[Some(0); 5];
+    for iter in 0..5 {
+        let number_to_add: u16 = {
+            ((iter * 1235) + 2) / (4 * 16)
+        };
+    }
+    
+}
