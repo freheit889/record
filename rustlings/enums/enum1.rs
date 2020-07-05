7644192e17e010839299737eb4a0f491ce86b98a
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 08:39:43 +0800

    Update enum1.rs

--- rustlings/enums/enum1.rs
+++ rustlings/enums/enum1.rs
@@ -1,1 +1,15 @@
+#[derive(Debug)]
+enum Message {
+    // TODO: define a few types of messages as used below
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
+}
 
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
+}
