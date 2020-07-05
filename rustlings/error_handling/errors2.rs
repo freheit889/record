c316aa55be254ddab0862f3422b2c248570e509d
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 20:10:51 +0800

    Create errors2.rs

---
+++ rustlings/error_handling/errors2.rs
@@ -0,0 +1,31 @@
+use std::num::ParseIntError;
+
+pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+    let processing_fee = 1;
+    let cost_per_item = 5;
+    //let qty = item_quantity.parse::<i32>();
+    match item_quantity.parse::<i32>(){
+        Ok(qty)=>Ok(qty * cost_per_item + processing_fee),
+        Err(ParseIntError)=>Err(ParseIntError)
+    }
+    
+    //Ok(qty * cost_per_item + processing_fee)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn item_quantity_is_a_valid_number() {
+        assert_eq!(total_cost("34"), Ok(171));
+    }
+
+    #[test]
+    fn item_quantity_is_an_invalid_number() {
+        assert_eq!(
+            total_cost("beep boop").unwrap_err().to_string(),
+            "invalid digit found in string"
+        );
+    }
+}
