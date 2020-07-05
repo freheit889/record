fab7cb36728565455f78397cd5debefda0f6ee36
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 23:58:03 +0800

    Create iterators4.rs

---
+++ rustlings/standard_library_types/iterators4.rs
@@ -0,0 +1,23 @@
+pub fn factorial(num: u64) -> u64 {
+    let result = (1..=num).fold(1, |acc, x| acc * x);
+
+    result
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(1, factorial(1));
+    }
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(2, factorial(2));
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(24, factorial(4));
+    }
+}
