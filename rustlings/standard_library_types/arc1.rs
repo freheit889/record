bdf198e86dd0486162c94043996090c449fc2150
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 22:56:54 +0800

    Create arc1.rs

---
+++ rustlings/standard_library_types/arc1.rs
@@ -0,0 +1,27 @@
+//这题有什么意义啊?还是我没搞明白
+#![forbid(unused_imports)] // Do not change this, (or the next) line.
+use std::sync::Arc;
+use std::thread;
+
+fn main() {
+    let numbers: Vec<_> = (0..100u32).collect();
+    let shared_numbers = Arc::new(1);// TODO
+    let mut joinhandles = Vec::new();
+    
+    let mut child_numbers=[1,2,3,4,5];
+    println!("{:?}",child_numbers);
+    for offset in 0..8 {
+        joinhandles.push(thread::spawn(move || {
+            let mut i = offset;
+            let mut sum = 0;
+            while i < child_numbers.len() {
+                sum += child_numbers[i];
+                i += 5;
+            }
+            println!("Sum of offset {} is {}", offset, sum);
+        }));
+    }
+    for handle in joinhandles.into_iter() {
+        handle.join().unwrap();
+    }
+}
