a0b9a7eb754d8bb484080c1bd243d151a9da0930
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 13:05:42 +0800

    Update strings2.rs

--- rustlings/String/strings2.rs
+++ rustlings/String/strings2.rs
@@ -10,3 +10,18 @@ fn main() {
 fn is_a_color_word(attempt: String) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
 }
+
+//或者  这里涉及到了隐式类型转换  String实现了Deref<Target=str> 所以在比较时被隐式的转换为了&str
+ 
+fn main() {
+    let word = "green"; // Try not changing this line :)
+    if is_a_color_word(word) {
+        println!("That is a color word I know!");
+    } else {
+        println!("That is not a color word I know.");
+    }
+}
+
+fn is_a_color_word(attempt: &str) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
+}
