dd96aefed8eedcb870926b140f8bdf46bb00d5dd
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 22:50:22 +0800

    Create box1.rs

---
+++ rustlings/standard_library_types/box1.rs
@@ -0,0 +1,36 @@
+#[derive(PartialEq, Debug)]
+pub enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn main() {
+    println!("This is an empty cons list: {:?}", create_empty_list());
+    println!("This is a non-empty cons list: {:?}", create_non_empty_list());
+    tests::test_create_empty_list();
+    tests::test_create_non_empty_list();
+}
+
+pub fn create_empty_list() -> List {
+    
+    List::Cons(10,Box::new(List::Nil))
+}
+
+pub fn create_non_empty_list() -> List {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_empty_list() {
+        assert_eq!(List::Nil, create_empty_list())
+    }
+
+    #[test]
+    fn test_create_non_empty_list() {
+        assert_ne!(create_empty_list(), create_non_empty_list())
+    }
+}
