1b7cdd68152bcb24c112ee256ab2ae4d905e96aa
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 20:20:53 +0800

    Create result1.rs

---
+++ rustlings/error_handling/result1.rs
@@ -0,0 +1,30 @@
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        if(value>0){
+            Ok(PositiveNonzeroInteger(value as u64))
+        }else if(value<0){
+            Err(CreationError::Negative)
+        }else{
+            Err(CreationError::Zero)
+        }
+
+    }
+}
+#[test]
+fn test_creation() {
+    assert!(PositiveNonzeroInteger::new(10).is_ok());
+    assert_eq!(
+        Err(CreationError::Negative),
+        PositiveNonzeroInteger::new(-10)
+    );
+    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+}
