fcf5196c0d7ea5caf1ceb80a7fbdaa867102dfcc
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 12:58:29 +0800

    Create strings2.rs

---
+++ rustlings/String/strings2.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let word = String::from("green"); // Try not changing this line :)
+    if is_a_color_word(word) {
+        println!("That is a color word I know!");
+    } else {
+        println!("That is not a color word I know.");
+    }
+}
+
+fn is_a_color_word(attempt: String) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
+}
