2c3965098070b130cdcbaabdde3de860eb7102fa
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-05 14:29:13 +0800

    Create primitive_types2.rs

---
+++ rustlings/primitive_types/primitive_types2.rs
@@ -0,0 +1,20 @@
+fn main() {
+
+    let my_first_initial = 'C';
+    if my_first_initial.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if my_first_initial.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+
+    let your_character='0';
+    if your_character.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if your_character.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+}
