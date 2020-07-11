e920539fa36ad56dc3f1f97c3831575eaf37ef00
freheit889 <36764640+freheit889@users.noreply.github.com>
2020-07-11 21:39:28 +0800

    Update threads1.rs

--- rustlings/threads/threads1.rs
+++ rustlings/threads/threads1.rs
@@ -1,4 +1,3 @@
-//有bug  没搞明白怎么写
 use std::sync::{Arc,Mutex};
 use std::thread;
 use std::time::Duration;
@@ -13,12 +12,12 @@ fn main() {
     let child=thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
-            let mut status_share=status_shared.lock().unwrap();
-            status_share.jobs_completed += 1;
+            let mut status_shared=status_shared.lock().unwrap();
+            (*status_shared).jobs_completed += 1;
         }
-    }).join();
-    
-    while status_shared.jobs_completed< 10 {
+    });
+    let status_shared= status.clone();
+    while status_shared.lock().unwrap().jobs_completed < 10 {
         println!("waiting... ");
         thread::sleep(Duration::from_millis(500));
     }
